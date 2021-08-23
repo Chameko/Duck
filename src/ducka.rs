fd3cea80907c56995ce86565819623bf13abbec2
Chameko <>
2021-08-23 22:04:03 +0800

    Added vertical split that rearranges windows approapriatly

--- src/ducka.rs
+++ src/ducka.rs
@@ -1,8 +1,41 @@
 use crate::window::{WindowSystem};
+use crossterm::event::{ Event, KeyCode, KeyEvent, KeyModifiers };
 
 // Manage entire app
 pub struct Duck {
     window_s: WindowSystem, // Holds the window system
     command_l: u16, // Holds the row to be used to enter command inputs
     status_l: u16, // Holds the row to be used to output
+}
+
+pub enum RawKey {
+    Char(char),
+    Up,
+    Down,
+    Left,
+    Right,
+    Enter,
+    Backspace,
+}
+
+pub enum KeyBinding {
+    Ctrl(RawKey),
+    Alt(RawKey),
+    Shift(RawKey),
+    F(u8),
+    Unsupported
+}
+
+impl Duck {
+    pub fn to_duck_input(event: Event) {
+
+    }
+
+    fn quit() {
+        std::process::exit(0)
+    }
+
+    fn remove() {
+
+    }
 }
--- src/window.rs
+++ src/window.rs
@@ -1,5 +1,5 @@
 #![warn(clippy::all, clippy::pedantic)]
-use petgraph::{Graph, prelude::*, Direction};
+use petgraph::{stable_graph::StableGraph, prelude::*, Direction};
 use crossterm::style::{Print, Stylize};
 use crossterm::cursor::MoveTo;
 use crossterm::{queue, execute};
@@ -19,7 +19,7 @@ pub enum WinType {
 pub struct Position(pub u16, pub u16);
 
 // Represents a split in the window
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 pub enum Split {
     Vertical(u16), // Percentage of the window the split takes
     Horizontal(u16), // Percentage of the window the split takes
@@ -34,12 +34,23 @@ pub struct Window {
 
 pub struct WindowSystem {
     focused: NodeIndex, // Holds the currently focused window
-    tree: Graph<Window, Split>, // Holds the graph containing the window tree
+    tree: StableGraph<Window, Split>, // Holds the graph containing the window tree
     size: (u16, u16),
 }
 
 
 impl WindowSystem {
+    // Render a specific window
+    pub fn render_window(&self, window: NodeIndex, stdout: &mut Stdout) {
+        let window = self.tree.node_weight(window).unwrap();
+
+        match window {
+            Window{win_t: WinType::Text(content), top_l, bottom_r} => WindowSystem::draw_c(String::from(content), top_l, bottom_r, stdout),
+            Window{win_t: WinType::Document(doc), top_l, bottom_r} => WindowSystem::draw_d(doc, top_l, bottom_r, stdout),
+            Window{win_t: WinType::Empty, ..} => (),
+        }
+    }
+
     // Render the entire screen
     pub fn render(&self, root: NodeIndex, stdout: &mut Stdout) {
         // For every node that isn't drawable it will call this function on. 
@@ -51,7 +62,7 @@ impl WindowSystem {
                 Window{win_t: WinType::Document(doc), top_l, bottom_r} => WindowSystem::draw_d(doc, top_l, bottom_r, stdout),
                 _ => println!("window type not implemented")
             }
-            match self.tree.edges_connecting(root, node).next().unwrap().weight() {
+            match self.tree.edge_weight(self.tree.find_edge(root, node).unwrap()).unwrap() {
                 Split::Vertical(col) => WindowSystem::draw_v(*col, (weight.top_l.1, weight.bottom_r.1), stdout),
                 Split::Horizontal(row) => WindowSystem::draw_h(*row, (weight.top_l.0, weight.bottom_r.0), stdout),
                 _ => println!("Split not supported")
@@ -73,7 +84,6 @@ impl WindowSystem {
         for lines in &doc.rows {
             if row >= row_l {
                 break;
-                execute!(stdout, MoveTo(0, size().unwrap().1), Print(format!("row: {:?} row_l: {:?}", row, row_l)));
             }
             for g in lines.string.graphemes(true).collect::<Vec<&str>>() {
                 if col >= col_l {
@@ -102,7 +112,7 @@ impl WindowSystem {
     }
 
     pub fn new_vertical(&mut self, win_t: WinType, win_t2: WinType, percentage: u16) -> (NodeIndex, NodeIndex) {
-        // Create a clone of the parent to use as adding a node mutable borrows it, should be safe as any changes made to parent are just adding the node and not modifying the wieghts
+        // Create a clone of the parent to use as adding a node mutable borrows it, should be safe as any changes made to parent are just adding the node and not modifying the weights
         let parent_top_l = self.tree.node_weight(self.focused).unwrap().top_l.clone();
         let parent_bottom_r = self.tree.node_weight(self.focused).unwrap().bottom_r.clone();
 
@@ -115,7 +125,10 @@ impl WindowSystem {
             win_t: win_t2,
             top_l: Position(parent_bottom_r.0 / percentage + 1, parent_top_l.1),
             bottom_r: parent_bottom_r.clone(),
-        }); 
+        });
+
+        self.tree.add_edge(self.focused, win_a, Split::Vertical(percentage));
+        self.tree.add_edge(self.focused, win_b, Split::Vertical(percentage));
 
         (win_a, win_b)
     }
@@ -136,14 +149,66 @@ impl WindowSystem {
             bottom_r: parent_bottom_r.clone(),
         });
 
+        self.tree.add_edge(self.focused, win_a, Split::Horizontal(percentage));
+        self.tree.add_edge(self.focused, win_b, Split::Horizontal(percentage));
+
         (win_a, win_b)
     }
 
-    pub fn root(tree: &mut Graph<Window, Split>, size: (u16, u16)) -> NodeIndex {
-        tree.add_node( Window {
+    pub fn root(&mut self, size: (u16, u16)) -> NodeIndex {
+        self.tree.add_node( Window {
             win_t: WinType::Empty,
             top_l: Position(0, 0),
             bottom_r: Position(size.0, size.1)
         })
+    }
+
+    pub fn vertical(&mut self, win_t: WinType, percentage: u16) -> NodeIndex {
+        let focused = self.focused;
+        let parent = self.get_parent(&focused);
+
+        // Create coppies of the various window positions
+        let parent_top_l = self.tree.node_weight(parent).unwrap().top_l.clone();
+        let parent_bottom_r = self.tree.node_weight(parent).unwrap().bottom_r.clone();
+        let f_win_top_l = self.tree.node_weight(focused).unwrap().top_l.clone();
+        let f_win_bottom_r = self.tree.node_weight(focused).unwrap().bottom_r.clone();
+
+        // Create the new window 
+        let n_win = self.tree.add_node(Window {
+            win_t,
+            top_l: parent_top_l.clone(),
+            bottom_r: Position(parent_bottom_r.0 / percentage - 1, parent_bottom_r.1)
+        });
+
+        // Create the container that parent will point to
+        let c_win = self.tree.add_node(Window {
+            win_t: WinType::Empty,
+            top_l: f_win_top_l.clone(),
+            bottom_r: f_win_bottom_r.clone()
+        });
+
+        let p_to_f = self.tree.find_edge(parent, focused).unwrap();
+
+        // Create edge between new container and parent
+        self.tree.add_edge(parent, c_win, self.tree.edge_weight(p_to_f).unwrap().clone());
+
+        // Remove edge between parent and focused
+        self.tree.remove_edge(p_to_f).unwrap();
+
+        // Add edges between container, the new window and the focused
+        self.tree.add_edge(c_win, n_win, Split::Vertical(percentage));
+        self.tree.add_edge(c_win, focused, Split::Vertical(percentage));
+
+        n_win
+    }
+
+    pub fn remove(&mut self, win: NodeIndex) -> NodeIndex {
+        let parent = self.get_parent(&win);
+        self.remove(win);
+        parent
+    }
+
+    pub fn get_parent(&mut self, win: &NodeIndex) -> NodeIndex {
+        self.tree.neighbors_directed(*win, Direction::Incoming).last().unwrap()
     }
 }
